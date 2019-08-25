bf98988a44c48baae834f5944e8e4ab5d524eb24
alanwhy <570028237@qq.com>
2019-08-25 21:42:35 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// ES Moudele 模块引入方式
+// CommonJS 模块引入规范
+// CMD
+// ADM
+// webpack 模块打包工具
+// js 模块打包工具 -> css png等
+
+// import Header from "./header.js"
+
+var Header = require("./header.js");
+
+// var style = require("./index.css");
+// import style from "./index.css"
+
+new Header()
