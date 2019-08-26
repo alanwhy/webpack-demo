9238f00d8f10d149bd0025e00e13e26bbab7e8b1
alanwhy <570028237@qq.com>
2019-08-26 22:07:46 +0800

    css-loader 样式文件的打包学习

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@
 
 // import Header from "./header.js"
 import avatar from "./avatar.jpg"
+import "./index.scss"
 
 var img = new Image();
 img.src = avatar;
+img.classList.add("avatar")
 
 var root = document.getElementById("root")
 root.append(img)
